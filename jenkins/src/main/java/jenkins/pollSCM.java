6be4fd3aa42c049c3a38711ac530017ed5c612ff
Haripriya Kulkarni <Haripriya Kulkarni@TYSS-Haripriya-Kulkarni>
2023-12-19 12:41:41 +0530

    pollscm

---
+++ jenkins/src/main/java/jenkins/pollSCM.java
@@ -0,0 +1,14 @@
+package jenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class pollSCM {
+	@Test
+	public void Scm() {
+		{
+			Reporter.log("hi",true);
+		}
+	}
+
+}
