2a33ec35c33b0f48a1d3b8ebfcad304ebd947c2a
Haripriya Kulkarni <Haripriya Kulkarni@TYSS-Haripriya-Kulkarni>
2023-12-19 13:18:19 +0530

    parameters

--- jenkins/src/main/java/jenkins/parametertest.java
+++ jenkins/src/main/java/jenkins/parametertest.java
@@ -1,5 +1,13 @@
 package jenkins;
 
+import org.testng.annotations.Test;
+
 public class parametertest {
+	@Test
+	public void test()
+	{
+		String BROWSER = System.getProperty("Browser");
+		System.out.println(BROWSER);
+	}
 
 }
